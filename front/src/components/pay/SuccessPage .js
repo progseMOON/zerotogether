e3f378b4f86fb0129c38ab7f7044d788e18d41da
MOON <progse0406@gmail.com>
2025-02-13 22:42:32 +0900

    basic start

---
+++ front/src/components/pay/SuccessPage .js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import './SuccessFailurePage.css'; // 스타일을 불러옵니다.
+
+const SuccessPage = () => {
+  const location = useLocation();
+  const { amount, memberInfo, response } = location.state;
+
+  return (
+    <div className="success-page">
+      <h1 className="title">😍후원 해주셔서 감사합니다!😍</h1>
+      <hr />
+      <p className="info">후원 금액: <span className="highlight">{amount} 원</span></p>
+      <p className="info">아이디: <span className="highlight">{memberInfo.memId}</span></p>
+      <p className="info">이름: <span className="highlight">{memberInfo.memName}</span></p>
+      <p className="info">이메일: <span className="highlight">{memberInfo.email}</span></p>
+      <p className="info">전화번호: <span className="highlight">{memberInfo.tel}</span></p>
+      <p className="info">주소: <span className="highlight">{memberInfo.addr1} {memberInfo.addr2}</span></p>
+      <a href="http://192.168.16.20:3000/mainpage" className="back-btn">메인 페이지로 돌아가기</a>
+    </div>
+  );
+};
+
+export default SuccessPage;
